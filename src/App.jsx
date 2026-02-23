e0c30d7185b285573d582e46451002db84d9a668
Marshallhch <marshallhch@gmail.com>
2026-02-23 17:29:15 +0900

    feat: Initialize project structure with a responsive navigation bar, GSAP animations, `react-scroll` integration, and core data constants.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import ReactLenis from 'lenis/react';
 import Navbar from './sections/Navbar';
 
 const App = () => {
   return (
-    <div className="relative w-screen min-h-screen">
+    <ReactLenis root className="relative w-screen min-h-screen">
       <Navbar />
-    </div>
+      <section id="home" className="h-screen bg-gray-500"></section>
+      <section id="services" className="h-screen bg-indigo-500"></section>
+      {/* <section id="about" className="h-screen bg-gray-500"></section> */}
+    </ReactLenis>
   );
 };
 
